7eacc050f539c862638208302f7a14844ee826cb
skyloongltl <skyloongltl@gmail.com>
2017-07-27 00:12:39 +0800

    first commit

---
+++ data/config.php
@@ -0,0 +1,12 @@
+<?php
+define('CONFIG', array(
+    'userConfig'        =>  array(
+        'cookie'            => 'q_c1=adc02bbaffc34fd5947555a70d904e43|1499238588000|1491479426000; d_c0="AIDCswSpkQuPTrAlILigvMiYBukzZJJMVkM=|1491533653"; _zap=58fc20de-693b-47d3-b5f1-8b1037b3868d; __utma=51854390.337722136.1492008424.1500529051.1500640514.16; __utmz=51854390.1494897227.11.6.utmcsr=zhihu.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmv=51854390.000--|2=registration_date=20160105=1^3=entry_date=20170406=1; q_c1=adc02bbaffc34fd5947555a70d904e43|1499238588000|1491479426000; r_cap_id="MjRjMDMwNDY5ZWI3NDk2NmFjODM3OTU0ODVhYTdiYmI=|1500529047|fb26fba335176328957412eee3ba49553c8f4896"; cap_id="NDgyMzRkMTJiOTU4NDdjZTk0YTg1ODQyMTAwMjdkMzA=|1500529047|2a6271e95e358ae562681ce58f24f03f1f2a177d"; l_cap_id="MmEyNjBjYTM5ZTg2NDFmZmIyNWQxMTQ0NjFjZmU1NTM=|1500529047|01b4cfce528a96d7c9c97c15c211b6573cfb6fce"; _xsrf=8eda2fa4d43031f045e558338897ac2c; __utmc=51854390; _xsrf=8eda2fa4d43031f045e558338897ac2c',
+        'follow_include' => 'data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics',
+    ),
+    'dbConfig'          =>  array(
+        'dbname'    =>  'zhihu',
+        'user'      =>  'root',
+        'password'  =>  '1490015807ltL',
+    ),
+));
---
+++ data/predis.php
@@ -0,0 +1,15 @@
+<?php
+class predis{
+
+    public static function getInstance()
+    {
+        static $instances = array();
+        $key = getmypid();
+        if (empty($instances[$key])) {
+            $instances[$key] = new Redis();
+
+            $instances[$key]->connect('127.0.0.1', '6379');
+        }
+        return $instances[$key];
+    }
+}
